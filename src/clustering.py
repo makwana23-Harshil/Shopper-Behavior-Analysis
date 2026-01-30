45f224d3d53faa85c740b7a487fdf7f33321d7e5
Harshil makwana <harshilmakwana2311@gmail.com>
2026-01-30 10:51:38 +0530

    Update clustering.py

--- src/clustering.py
+++ src/clustering.py
@@ -1,11 +1,6 @@
-import pandas as pd
 from sklearn.cluster import KMeans
 
-df = pd.read_csv("data/processed_data.csv")
-
-kmeans = KMeans(n_clusters=4, random_state=42)
-df["Cluster"] = kmeans.fit_predict(df)
-
-df.to_csv("data/clustered_data.csv", index=False)
-print("✅ Clustering completed")
-
+def perform_clustering(df):
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    df["Cluster"] = kmeans.fit_predict(df)
+    return df, kmeans
